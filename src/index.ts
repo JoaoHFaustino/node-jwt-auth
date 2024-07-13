7edcb4d746d660f77109ffd149df3d71b1492f99
João Henrique Faustino <40185240+Joaohenrique15@users.noreply.github.com>
2024-07-13 11:46:57 -0300

    chore: adding route to generate and and validate the token

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
-import express from 'express';
+import express, { json } from 'express';
+import { generateToken, authenticateToken } from './middlewares/auth';
 
 const app = express();
 const port = 3000;
 
+app.use(json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!!');
 });
+
+app.post('/generate-token', (req, res) => {
+    const { username } = req.body;
+    const user = { id: 1, username };
+    const token = generateToken(user);
+  
+    res.json({ token });
+  });
+app.get('/protected', authenticateToken, (req, res) => {
+    res.send('This is a protected route');
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
